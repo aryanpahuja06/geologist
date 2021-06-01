0291c8423f72a0aefb498e3cbace71c8138dd52f
aryanpahuja06 <81355135+aryanpahuja06@users.noreply.github.com>
2021-06-01 15:07:36 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,30 @@
+class Iron{
+	constructor(x,y,w,h)
+	{
+		var options={
+			restitution:0.3,
+			friction:5,
+			density:1
+		}
+		this.x=x;
+		this.y=y;
+		this.width =w
+        this.height=h
+		this.body = Bodies.rectangle(this.x, this.y, this.width,this.height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("brown");
+		rect(0,0,this.width, this.height);
+			pop()
+	}
+
+}
